f8f091680d6010689e326e8b3d3bc94d32f008b7
Jeno Pekarjuk <epekaryuk@outlook.com>
2026-08-29 20:31:34 +0200

    Console app functionality & basic error handling

--- DataProcessor/Program.cs
+++ DataProcessor/Program.cs
@@ -2,4 +2,31 @@
 
 var dp = new DataProcessor.Core.DataProcessor(dataFilePath);
 
-var path = dp.GetItemPath("Allen Wrench Set");
+Console.WriteLine("Available items:\n");
+
+for (var i = 0; i < dp.Items.Count(); i++)
+{
+    var item = dp.Items.ElementAt(i);
+    Console.WriteLine($"[{i + 1}] - {item}");
+}
+
+Console.WriteLine("\nWhat item would you like to search for?");
+
+var userInput = string.Empty;
+var parseSucceded = false;
+int parsedIndex = 0;
+
+while (string.IsNullOrWhiteSpace(userInput) || !parseSucceded)
+{
+    Console.WriteLine("Please enter proper number");
+    userInput = Console.ReadLine();
+    parseSucceded = int.TryParse(userInput, out var itemIndex);
+    parsedIndex = itemIndex;
+}
+
+Console.WriteLine();
+
+var path = dp.GetItemPath(dp.Items.ElementAt(parsedIndex - 1));
+
+foreach (var step in path)
+    Console.WriteLine($"{step}");
