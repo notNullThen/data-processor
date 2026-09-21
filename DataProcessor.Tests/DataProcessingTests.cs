1ac75a120d9bc105b0c1dea2554bd3d9508ac993
Jeno Pekarjuk <epekaryuk@outlook.com>
2026-09-21 23:45:03 +0200

    basic data processing tests

---
+++ DataProcessor.Tests/DataProcessingTests.cs
@@ -0,0 +1,41 @@
+﻿using DataProcessor.Core;
+using DataProcessor.Tests.TestData;
+
+namespace DataProcessor.Tests;
+
+public class DataProcessingTests
+{
+    public static TheoryData<string[]> ValidDataSets =>
+        [DataMedium.Value, DataDifferentIdent.Value];
+
+    public static TheoryData<string[]> InvalidData => [DataCorrupted.Value];
+
+    [Theory]
+    [MemberData(nameof(ValidDataSets))]
+    public void FindsCorrectPathToDeeplyNestedItem(string[] lines)
+    {
+        string[] expectedPath =
+        [
+            "Enter the building lobby.",
+            "Go to the east hallway.",
+            "Enter the bedroom.",
+            "Check under the bed.",
+        ];
+
+        var dp = new DataProcessorCore(lines);
+
+        var actualPath = dp.GetItemPath("Shoe Box");
+
+        Assert.Equal(expectedPath, actualPath);
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidData))]
+    public void ThrowsErrorWhenReadingCorruptedFile(string[] lines)
+    {
+        Assert.Throws<InvalidDataException>(() =>
+        {
+            new DataProcessorCore(lines);
+        });
+    }
+}
